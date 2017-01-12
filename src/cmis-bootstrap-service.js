7713c7eb784d36855d23c932892db586aebdb5e5
Gavin Cornwell <gavin.cornwell@alfresco.com>
2017-01-12 06:21:31 +0000

    Created SAM template for stack (only including bootstrap lambda initially) and re-factored bootstrap lambda to use input from proxy API gateway call.

--- src/cmis-bootstrap-service.js
+++ src/cmis-bootstrap-service.js
@@ -7,5 +7,24 @@ module.exports.handler = (event, context, callback) => {
 
   console.log("Bootstrap service received event: " + JSON.stringify(event, null, 2));
 
-  cmis.addRepository(event.repositoryId, event.baseUrl, callback);
+  // get repositoryId from body
+  var repositoryId = JSON.parse(event.body).repositoryId;
+
+  // construct the baseUrl
+  // TODO: extract the region into an environment variable
+  var baseUrl = "https://" + event.requestContext.apiId + ".execute-api.eu-central-1.amazonaws.com/" + event.requestContext.stage;
+
+  cmis.addRepository(repositoryId, baseUrl, function(error, data) {
+    if (error) {
+      callback(error);
+    } else {
+      var response = {
+        statusCode: 201,
+        body: JSON.stringify(data)
+      };
+
+      console.log("Bootstrap service result: " + JSON.stringify(response, null, 2));
+      callback(null, response);
+    }
+  });
 };
